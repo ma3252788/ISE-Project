bea4f772396f0976fe80d4441de8dfc864c534e7
Arvin <tianbing010@126.com>
2017-01-12 01:47:18 +0800

    新的

---
+++ Part2-RS422/uart.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:45:45 01/11/2017 
+// Design Name: 
+// Module Name:    uart 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module uart(clk59m,rst,txd,rxd,send_over,dout
+    );
+input clk59m;
+input rst;
+input rxd;
+
+output txd;
+output send_over;
+output dout;
+
+
+endmodule
