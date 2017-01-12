7d9b240f04db3f041b9a7ea18b6f8f7db3696c4a
Arvin <tianbing010@126.com>
2017-01-12 10:13:00 +0800

    新建第二个工程

--- UART/uart.v
+++ UART/uart.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date:    22:45:45 01/11/2017 
+// Create Date:    10:12:40 01/12/2017 
 // Design Name: 
 // Module Name:    uart 
 // Project Name: 
@@ -18,31 +18,8 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module uart(clk59m,rst,txd,rxd,send_over,dout
+module uart(
     );
-	 
-//设置输入输出
-input clk59m;
-input rst;
-input rxd;
-
-output txd;
-output send_over;
-output dout;
-
-
-wire txd;//将txd和rxd设为线网型
-wire rxd;
-
-wire samp_baud;//生成采样频率模块  
-
-
-
-
-
-
-
-
 
 
 endmodule
